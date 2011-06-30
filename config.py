57d5f589d30c42084a29e18d5fdd82afc927f493
piernov <piernov@32555feb-acdc-9533-d4e0-d00e80c872cf>
2011-06-30 12:42:39 +0000

    Added python_bot in branches/
    git-svn-id: https://suckerserv.googlecode.com/svn/branches/python_bot@177 32555feb-acdc-9533-d4e0-d00e80c872cf

---
+++ config.py
@@ -0,0 +1,33 @@
+irc_nickname = 'Suckerbot'
+
+irc_identity = {
+    'gamesurge': {
+        'host': 'irc.gamesurge.net',
+        'port': 6667,
+        'channels': (
+            '#suckerserv',
+        ),
+        'line_rate': 2.0,
+		'default_channel': '#suckerserv',
+    },
+}['gamesurge']
+
+servers = {
+    'server1': {
+        'host':          'localhost',
+        'port':           28795,
+        'password':       '',
+        'format':         '\00301%s\003',
+        'reconnect_time': 60,
+    },
+    'server2': {
+        'host':          'localhost',
+        'port':           30010,
+        'password':       '',
+        'format':         '\00307%s\003',
+        'reconnect_time': 60,
+    }
+}
+
+stats_url = ''
+
