46e0abffd6bc2404d7860ad1951ba5cc2c65ab28
piernov <piernov@32555feb-acdc-9533-d4e0-d00e80c872cf>
2013-11-30 14:03:38 +0000

    Improve info and players python_bot commands
    git-svn-id: https://suckerserv.googlecode.com/svn/trunk/python_bot@599 32555feb-acdc-9533-d4e0-d00e80c872cf

--- commands.py
+++ commands.py
@@ -1,19 +1,24 @@
 server = {
-    'ping':     lambda server_write, **_: server_write('code:sendmsg("pong!")'),
+	'ping': lambda server_write, **_: server_write('code:sendmsg("pong!")'),
 	'players': lambda server_write, **_: server_write('\
-		      code:local msg = ""\
-		      for i, cn in ipairs(server.clients()) do\
-			  msg = msg .. server.player_name(cn) .. " "\
-		      end\
-		      if msg == "" then\
-			  sendmsg("no players connected")\
-		      else\
-			  sendmsg(msg)\
-		      end\
-	'),
+		code:local msg = "" \
+			for p in server.gplayers() do \
+				if server.player_priv_code(p.cn) >= server.PRIV_ADMIN then \
+					msg = msg .. irc_color_orange(string.format("%s(%d)", p:name(), p.cn)) \
+				elseif server.player_priv_code(p.cn) >= server.PRIV_MASTER then \
+					msg = msg .. irc_color_green(string.format("%s(%d)", p:name(), p.cn)) \
+				else \
+					msg = msg .. string.format("%s(%d) ", p:name(), p.cn) \
+				end \
+			end \
+			if msg == "" then\
+				sendmsg("no players connected")\
+			else\
+				sendmsg(msg)\
+			end\
+		'),
 	'restart': lambda server_write, **_: server_write('code:sendmsg("restarting server..."); server.restart_now()'),
 	'uptime': lambda server_write, **_: server_write('code:sendmsg("Uptime: " .. server.format_duration(server.uptime))'),
-	'info': lambda server_write, **_: server_write('code:sendmsg(string.format("Gamemode: %s Map: %s Players: %i Time left: %i Mastermode: %i", server.gamemode, server.map, server.playercount, server.timeleft, server.mastermode))')
 }
 
 def f(server_write, args, nickname, **_):
@@ -211,3 +216,14 @@ def f(server_write, args, **_):
                         sendmsg(cube2irc_colors(server.clearbans_message))\
                         ''')
 server['clearbans'] = f
+
+def f(server_write, args, **_):
+	server_write('''\
+		code:sendmsg(\
+			string.format(\
+				irc_color_green("Gamemode:") .." %s " .. irc_color_grey("|") .. irc_color_green("Map:") .. " %s " .. irc_color_grey("|") .. irc_color_green("Players:") .. " %s " .. irc_color_grey("|") .. irc_color_green("Time left:") .. " %s min " .. irc_color_grey("|") .. irc_color_green("Mastermode:") .. " %s", \
+				irc_color_blue(server.gamemode), irc_color_blue(server.map), irc_color_blue(server.playercount), irc_color_blue(server.timeleft), irc_color_blue(server.mastermode)\
+			)\
+		)\
+	''')
+server['info'] = f
